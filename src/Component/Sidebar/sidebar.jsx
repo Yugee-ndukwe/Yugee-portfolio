84150cd8cb9079213989c09cc1ad21a3e8339f4f
ugonma ndukwe <ugonmandukwe30@gmail.com>
2024-05-31 16:12:34 +0100

    i made some changeson the navbar

--- src/Component/Sidebar/sidebar.jsx
+++ src/Component/Sidebar/sidebar.jsx
@@ -3,7 +3,6 @@ import LOGO from '../../assets/yugee.png';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 
 export function Sidebar() {
@@ -16,20 +15,32 @@ export function Sidebar() {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="m-auto">
-                        <NavLink exact="true" to="/" className="mx-5 nav-item" activeClassName="active">
+                    <Nav className="">
+                        <NavLink
+                            exact="true"
+                            to="/"
+                            className={({ isActive }) => isActive ? "mx-5 nav-item active" : "mx-5 nav-item"}
+                        >
                             <div className="icon-with-text">
                                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" className="nav-icon home-icon" />
                                 <span className="icon-text">Home</span>
                             </div>
                         </NavLink>
-                        <NavLink exact="true" to="/about" className="mx-5 nav-item" activeClassName="active">
+                        <NavLink
+                            exact="true"
+                            to="/about"
+                            className={({ isActive }) => isActive ? "mx-5 nav-item active" : "mx-5 nav-item"}
+                        >
                             <div className="icon-with-text">
                                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" className="nav-icon about-icon" />
                                 <span className="icon-text">About</span>
                             </div>
                         </NavLink>
-                        <NavLink exact="true" to="/contact" className="mx-5 nav-item" activeClassName="active">
+                        <NavLink
+                            exact="true"
+                            to="/contact"
+                            className={({ isActive }) => isActive ? "mx-5 nav-item active" : "mx-5 nav-item"}
+                        >
                             <div className="icon-with-text">
                                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" className="nav-icon contact-icon" />
                                 <span className="icon-text">Contact</span>
